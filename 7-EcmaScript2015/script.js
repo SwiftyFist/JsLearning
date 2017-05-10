37e1521caeacc4ff4166f627177119f99cafa7fe
SwiftyFist <liquidc93@gmail.com>
2017-05-10 21:20:03 +0200

    Inizio lecture 7

---
+++ 7-EcmaScript2015/script.js
@@ -0,0 +1,11 @@
+/*jslint vars: true, plusplus: true, devel: true, maxerr: 50, browser: true, esversion: 6*/
+/*global define */
+
+//var john = ['John', 26];
+//var namee = john[0];
+//var age = john[1];
+
+const [namee, age] = ['John', 26];
+
+console.log(name);
+console.log(age);
