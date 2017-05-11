89cc2f2e861ef396a504396ac1e7f91c768ca4fd
SwiftyFist <liquidc93@gmail.com>
2017-05-11 22:25:45 +0200

    nulla di che

--- 7-EcmaScript2015/script.js
+++ 7-EcmaScript2015/script.js
@@ -1,11 +1,7 @@
-/*jslint vars: true, plusplus: true, devel: true, maxerr: 50, browser: true, esversion: 6*/
-/*global define */
-
-//var john = ['John', 26];
-//var namee = john[0];
-//var age = john[1];
-
-const [namee, age] = ['John', 26];
-
-console.log(name);
-console.log(age);
+function SmithPerson(parameters) {
+    let {firstName, yearOfBirth, lastName = 'Smith', nationality = 'american'} = parameters;
+    this.firstNAme = firstName;
+    this.lastName = lastName;
+    this.yearOfBirth = yearOfBirth;
+    this.nationality = nationality;
+}
